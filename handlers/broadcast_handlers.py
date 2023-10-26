a858adde5e0257ec57a72b6fc6d2ce40830a13ff
Dhanu990 <130265533+Dhanu990@users.noreply.github.com>
2023-10-26 12:12:09 +0530

    Update broadcast_handlers.py

--- handlers/broadcast_handlers.py
+++ handlers/broadcast_handlers.py
@@ -18,8 +18,7 @@ broadcast_ids = {}  # Initialize the broadcast_ids dictionary
 
 async def main_broadcast_handler(m, db):
     # Your existing send_msg function
-
-async def main_broadcast_handler(m, db):
+    
     all_users = await db.get_all_users()
     broadcast_msg = m.reply_to_message
     while True:
@@ -41,30 +40,34 @@ async def main_broadcast_handler(m, db):
         success=success
     )
     async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-        async for user in all_users:
-            sts, msg = await send_msg(
-                user_id=int(user['id']),
-                message=broadcast_msg
-            )
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            if sts == 200:
-                success += 1
-            else:
-                failed += 1
-            if sts == 400:
-                await db.delete_user(user['id'])
-            done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(
-                    dict(
-                        current=done,
-                        failed=failed,
-                        success=success
-                    )
+        for user in all_users:
+            try:
+                sts, msg = await send_msg(
+                    user_id=int(user['id']),
+                    message=broadcast_msg
                 )
+                if msg is not None:
+                    await broadcast_log_file.write(msg)
+                if sts == 200:
+                    success += 1
+                else:
+                    failed += 1
+                if sts == 400:
+                    await db.delete_user(user['id'])
+                done += 1
+                if broadcast_ids.get(broadcast_id) is None:
+                    break
+                else:
+                    broadcast_ids[broadcast_id].update(
+                        dict(
+                            current=done,
+                            failed=failed,
+                            success=success
+                        )
+                    )
+            except Exception as e:
+                # Handle exceptions here
+                traceback.print_exc()
     if broadcast_ids.get(broadcast_id):
         broadcast_ids.pop(broadcast_id)
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
@@ -82,4 +85,3 @@ async def main_broadcast_handler(m, db):
             quote=True
         )
     await aiofiles.os.remove('broadcast.txt')
-    
